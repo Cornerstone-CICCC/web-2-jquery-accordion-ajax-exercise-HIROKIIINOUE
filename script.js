5d083fac6c6f2154597653e08173d04dce24fe8f
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-11-26 23:25:48 -0800

    done exercise

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-$(function() {
+$(function () {
   // your code here
-})
+  $(".accordion-header").on("click", function () {
+    $(this).next(".accordion-content").slideToggle();
+  });
+
+  $("button").on("click", function () {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      method: "GET",
+      success: function (data) {
+        data.todos.forEach((todo) => {
+          $("ul").append(`<li>${todo.todo}</li>`);
+        });
+      },
+      error: function (err) {
+        console.error(err);
+      },
+    });
+  });
+});
